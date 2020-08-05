d507bd830d8ce7f3584c136ced1f823eea7c34e6
Yesid <trigoperfec3@hotmail.com>
2020-08-05 10:00:41 -0500

    commit inicial

---
+++ Calculadora/app/src/main/java/com/mitiempo/calculadora/Presentacion/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mitiempo.calculadora.Presentacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mitiempo.calculadora.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
